0b537a118ea131adc29a54fe93565f5b18279e25
Thibaud-Guadagna <thibaud.guadagna@gmail.com>
2025-04-22 19:06:02 +0200

    Fix NavBar et Hover

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,16 @@
-
+import { Outlet } from "react-router";
 import Footer from "./components/Footer";
 import Header from "./components/header";
 
 function App() {
   return (
     <>
-
-        <Header />
-    
-        <Footer />
-  
+      <Header />
+      <div>
+        <Outlet />
+      </div>
+      <Footer />
     </>
-
   );
 }
 
