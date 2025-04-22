b09b5c697612dfb02a3be2fdde20c0c4337ae198
Thibaud-Guadagna <thibaud.guadagna@gmail.com>
2025-04-22 16:40:54 +0200

    Ajout du composant header avec le composant menu Burger

---
+++ client/src/components/burger.tsx
@@ -0,0 +1,33 @@
+type BurgerProps = {
+  toggleMenu: () => void;
+  isOpen: boolean;
+};
+
+function Burger({ toggleMenu, isOpen }: BurgerProps) {
+  return (
+    <section className="flex flex-col items-end mt-4 z-50 relative bg-[#2c7865]/70 rounded-sm h-9 w-10 cursor-pointer p-1">
+      <div onClick={toggleMenu} onKeyDown={toggleMenu}>
+        <div className="relative w-8 h-6">
+          <span
+            className={`absolute left-0 h-1 w-8  bg-[#d9bf77] transform transition-all duration-300 ease-in-out ${
+              isOpen ? "rotate-45 top-2.5" : "top-0"
+            }`}
+          />
+
+          <span
+            className={`absolute left-0 h-1 w-8  bg-[#d9bf77] transition-all duration-300 ease-in-out ${
+              isOpen ? "opacity-0" : "top-2.5"
+            }`}
+          />
+          <span
+            className={`absolute left-0 h-1 w-8  bg-[#d9bf77] transform transition-all duration-300 ease-in-out ${
+              isOpen ? "-rotate-45 top-2.5" : "top-5"
+            }`}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Burger;
