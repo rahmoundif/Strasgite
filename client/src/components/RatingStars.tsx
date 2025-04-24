19e514a97f8433be4a7be38fdd33cbfb86a37904
Thibaud-Guadagna <thibaud.guadagna@gmail.com>
2025-04-24 17:59:20 +0200

    Ajout du composant Avis avec début de style sur le calendrier

--- client/src/components/RatingStars.tsx
+++ client/src/components/RatingStars.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
 function RatingStars() {
-  const [Rated, setRated] = useState(6);
+  const [Rated, setRated] = useState(0);
   const toggleRating = (star: number) => {
-    setRated((prev) => (prev === star ? 6 : star));
+    setRated((prev) => (prev === star ? 0 : star));
   };
   return (
     <section className="flex space-x-0.5 mb-1">
@@ -13,7 +13,7 @@ function RatingStars() {
           key={star}
           onClick={() => toggleRating(star)}
           className={`transition transform duration-200 hover:scale-110 focus:scale-110  cursor-pointer
-        ${star >= Rated ? "text-yellow-300" : "text-gray-400"}
+        ${star <= Rated ? "text-yellow-300" : "text-gray-400"}
         
    `}
         >
@@ -22,14 +22,14 @@ function RatingStars() {
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            fill={star >= Rated ? "currentColor" : "none"}
+            fill={star <= Rated ? "currentColor" : "none"}
             stroke="currentColor"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
           >
             <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-            <title>{star >= Rated ? "Rated Star" : "Default Star"}</title>
+            <title>{star <= Rated ? "Rated Star" : "Default Star"}</title>
             {/* equivalent au ALT pour une image*/}
           </svg>
         </button>
